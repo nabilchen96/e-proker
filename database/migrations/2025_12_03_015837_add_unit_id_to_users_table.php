a3600d5acd608beb2e4455a7b2860bc53e7214df
wahidalqorni <idunalqorni@gmail.com>
2025-12-03 09:01:20 +0700

    tambah unit_id di users

--- database/migrations/2025_12_03_015837_add_unit_id_to_users_table.php
+++ database/migrations/2025_12_03_015837_add_unit_id_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreignId('unit_id')->constrained('units')->onDelete('cascade')->after('password')->nullable();
+            $table->bigInteger('unit_id')->after('password')->nullable();
         });
     }
 
